4fa077ec828b5c6b03a130c8d6e447b9c4f2a738
Daniel Friesel <derf@finalrewind.org>
2013-03-15 22:05:24 +0100

    fading demo code using soft pwm

--- main.c
+++ main.c
@@ -5,34 +5,66 @@
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 
-void set_led(char red, char green, char blue){
+volatile struct {
+	unsigned blue:1;
+	unsigned green:1;
+	unsigned red:1;
+} ledstate;
+
+void set_led(void){
 	// in difference to the schematic, PB4 is red, PB1 is green and PB3 is blue
 	// not because of common cathode
-	PORTB = (!red << PB4) | (!green << PB1) | (!blue << PB3);
+	PORTB = (!ledstate.red << PB4) | (!ledstate.green << PB1)
+		| (!ledstate.blue << PB3);
 }
 
 int main(void){
 	
-//	uint16_t softpwm = 0;
-	uint8_t r=0, g=85, b=170;
-	
-	DDRB = _BV(DDB1) | _BV(DDB3) | _BV(DDB4);	
-	PORTB = 0xFF;
-	
-	
-/*	TCCR1 = _BV(COM1A1) | _BV(COM1A0) | _BV(PWM1A) | _BV(CS10);
-	TCCR0A = _BV(WGM01) | _BV(WGM00);
-	GTCCR = _BV(COM1B1) | _BV(COM1B0) | _BV(PWM1B);
-		
+	DDRB = _BV(DDB1) | _BV(DDB3) | _BV(DDB4);
 
-	OCR1A = 255;
-	OCR1C = 255; 
-	OCR1B = 127;
-*/	
-	while(1){
-		
-	//	set_led(1,1,1);	
-	//	_delay_ms(1000);	
+	uint16_t fadecnt = 0;
+
+	uint8_t cnt = 0;
+	uint8_t want_blue = 0;
+	uint8_t want_green = 0;
+	uint8_t want_red = 10;
+
+	uint8_t is_blue, is_green, is_red;
+
+	while (1) {
+		cnt++;
+		if (cnt == 0) {
+			ledstate.red = ledstate.green = ledstate.blue = 1;
+			if (++fadecnt == 2048) {
+				fadecnt = 0;
+				want_blue = rand();
+				want_green = rand();
+				want_red = rand();
+			}
+			if ((fadecnt % 8) == 0) {
+				if (is_red > want_red)
+					is_red--;
+				if (is_red < want_red)
+					is_red++;
+				if (is_green > want_green)
+					is_green--;
+				if (is_green < want_green)
+					is_green++;
+				if (is_blue > want_blue)
+					is_blue--;
+				if (is_blue < want_blue)
+					is_blue++;
+			}
+		}
+		if (cnt == is_blue)
+			ledstate.blue = 0;
+		if (cnt == is_green)
+			ledstate.green = 0;
+		if (cnt == is_red)
+			ledstate.red = 0;
+
+		set_led();
 	}
+
 	return 0;
 }
