a3c2d5da6b1dd68f6706f0b67db57e5dd62faee7
Daniel Friesel <derf@finalrewind.org>
2013-09-09 19:03:39 +0200

    use __flash feature instead of pgmspace.h

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 #include <avr/eeprom.h>
 #include <avr/io.h>
 #include <stdlib.h>
-#include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 
 /*
@@ -81,7 +80,7 @@ uint8_t ee_valid EEMEM;
 uint8_t ee_seq[SEQ_MAX + 1] EEMEM;
 uint8_t ee_addrhi, ee_addrlo, ee_addri2c EEMEM;
 
-const uint8_t pwmtable[32] PROGMEM = {
+static const __flash uint8_t pwmtable[32] = {
 	0, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 10, 11, 13, 16, 19, 23,
 	27, 32, 38, 45, 54, 64, 76, 91, 108, 128, 152, 181, 215, 255
 };
